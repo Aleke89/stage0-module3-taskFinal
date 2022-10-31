fce46c580db42fab7e11a308dc33cb725ec367c2
Aleke <alnur.rysbek@mail.ru>
2022-10-31 10:19:12 +0600

    success

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
+
+
 public class NumberReverter {
     public void revert(int number) {
+        System.out.println((number%10)+""+(number%100/10)+""+(number/100));
     }
 }
